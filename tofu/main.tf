d99134b48c87abf5dc1f3111f55b02023287fee6
Sam Rivera <sam@example.com>
2026-09-26 04:32:53 +0800

    Shorten the OpenTofu example

--- tofu/main.tf
+++ tofu/main.tf
@@ -1,12 +1,7 @@
 terraform {
-  required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 6.66" }
-  }
+  required_providers { aws = { source = "hashicorp/aws", version = "~> 6.66" } }
 }
-
-provider "aws" {
-  region = "us-east-1" # the endpoint comes from AWS_ENDPOINT_URL
-}
+provider "aws" { region = "us-east-1" } # the endpoint comes from AWS_ENDPOINT_URL
 
 resource "aws_dynamodb_table" "books" {
   name         = "booknest-books-tofu"
